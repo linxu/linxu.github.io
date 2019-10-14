6aec371ce2465f278f37c55069bd01955eeffd77
linxu <linxu516@qq.com>
2019-10-14 11:37:10 +0800

    i18n

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import App from './App.vue'
+import en from '@/lang/en'
+import zh from '@/lang/zh'
 
 Vue.config.productionTip = false
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages: {
+    en: en,
+    zh: zh
+  }
+})
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
