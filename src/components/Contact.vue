059aae68bfb7b98ceb95ed8c07b8d7b6eb35ccb8
linxu <linxu516@qq.com>
2019-11-02 21:49:19 +0800

    update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -55,7 +55,7 @@ import axios from "axios";
 
 Vue.use(Toast);
 
-axios.defaults.baseURL = "http://122.51.87.166:8888";
+axios.defaults.baseURL = "https://122.51.87.166:8888";
 
 export default {
   name: "Contact",
